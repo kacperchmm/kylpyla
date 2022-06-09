fa19b822d5ada96c80e3be886c7e1b81484ebe5f
kacperchmm <kacperchmm@gmail.com>
2022-06-09 20:23:42 +0200

    Display with movement

---
+++ main/GamePanel.java
@@ -0,0 +1,132 @@
+package main;
+
+import java.lang.System;
+import java.util.Currency;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+
+
+public class GamePanel extends JPanel implements Runnable{
+    final int originalTileSize = 16; // 16x16
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //48x48
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768
+    final int screenHeight = tileSize * maxScreenRow; //576
+
+    int fps = 60;
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    int playerX = 100, playerY = 100, playerSpeed = 4;
+
+    public GamePanel(){
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);    
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    // public void run(){
+
+    //     double drawInterval = 1000000000/fps;
+    //     double nextDrawTime = System.nanoTime() + drawInterval;
+        
+    //     while(gameThread != null){
+
+    //         update();
+
+    //         repaint();
+
+    //         try{
+    //             double remainingTime = nextDrawTime - System.nanoTime();
+    //             remainingTime = remainingTime / 1000000;
+                
+    //             if(remainingTime < 0){
+    //                 remainingTime = 0;
+    //             }
+
+    //             Thread.sleep((long)remainingTime);
+
+    //             nextDrawTime += drawInterval;
+
+    //         } catch (InterruptedException e){
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
+
+    public void run(){
+
+        double drawInterval = 1000000000/fps;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+        
+        while(gameThread != null){
+
+            currentTime = System.nanoTime();
+            timer += (currentTime - lastTime);
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update(){
+
+        if(keyH.upPressed){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed){
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed){
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g){
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
+}
