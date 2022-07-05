d0cf55db684c112ae4a455d58483c0d57f0f63f2
Kacper <kacperchmm@gmail.com>
2022-07-05 05:31:46 +0200

    Camera, collisions and objects v2

---
+++ source/main/AssetSetter.java
@@ -0,0 +1,32 @@
+package main;
+
+import object.OBJ_chest;
+import object.OBJ_door;
+import object.OBJ_key;
+
+public class AssetSetter{
+	
+	GamePanel gp;
+	public AssetSetter(GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	public void setObject() {
+		
+		gp.obj[0] = new OBJ_key();
+		gp.obj[0].worldX = 12 * gp.tileSize;
+		gp.obj[0].worldY = 33 * gp.tileSize;
+		
+		gp.obj[1] = new OBJ_key();
+		gp.obj[1].worldX = 36 * gp.tileSize;
+		gp.obj[1].worldY = 8 * gp.tileSize;
+		
+		gp.obj[2] = new OBJ_chest();
+		gp.obj[2].worldX = 10 * gp.tileSize;
+		gp.obj[2].worldY = 8 * gp.tileSize;
+		
+		gp.obj[3] = new OBJ_door();
+		gp.obj[3].worldX = 10 * gp.tileSize;
+		gp.obj[3].worldY = 11 * gp.tileSize;
+	}
+}
---
+++ source/object/OBJ_chest.java
@@ -0,0 +1,20 @@
+package object;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class OBJ_chest extends SuperObject{
+	
+	public OBJ_chest() {
+		
+		name = "chest";
+		
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/chest.png"));
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
